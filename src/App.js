2f5f7a845cc8f5a20d82d9a61fdbabdb6be71c4c
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2019-11-18 11:26:24 +0400

    Add HomeW_8

--- src/App.js
+++ src/App.js
@@ -2,82 +2,67 @@ import React, { Component } from 'react'
 import './App.css'
 import './w3.css'
 import { ShowWeather } from './Components/ShowWeather'
-import { AddNewPlace } from './Components/AddNewPlace'
+import  AddNewPlace from './Components/AddNewPlace'
+import { connect } from 'react-redux';
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = {
+     this.state = {
       activePlace: 0,
-      addPlace: 0,
-      places: this.props.places
-
-    }
-    this.handlePlaceSubmit = this.handlePlaceSubmit.bind(this)
-  }
-
-  handlePlaceSubmit (place) {
-    const updatedPlaces = this.state.places.concat(place)
-    this.setState(
-      { places: updatedPlaces }
-    )
+      addPlace: 0
+     } 
   }
 
   render () {
     const activePlace = this.state.activePlace
-
     return (
       <div className="App ">
 
         <div key="cont001" className= "w3-sidebar w3-green w3-bar-block">
           <h2> Погода </h2>
-
-          {this.state.places.map((place, zip) => (
-
+          {this.props.zip.map((zip,id) => (
             <button
               className="w3-bar-item w3-button tablink"
-              key={zip}
+              key={id}
               onClick={() => {
                 this.setState({
-                  activePlace: zip,
+                  activePlace: id,
                   addPlace: 0
                 })
-              }}
-            >
-              {place.city}
+              }} >
+              {this.props.city[id]}
             </button>
-
           ))}
-
           <button
-            className="w3-text-blue w3-grey w3-bar-item w3-button tablink "
+            className="w3-text-red w3-bar-item w3-button tablink "
             key= "addPlace"
             onClick={() => {
               this.setState({ addPlace: 1 })
-            }}
-          >
+            }}>
             Новый город
           </button>
         </div>
-
-
-
          {(this.state.addPlace === 0)
           ? <ShowWeather
             className="w3-container"
             key = {activePlace}
-            zip={this.state.places[activePlace].zip} />
+            zip=  {this.props.zip[activePlace]} />
           : <AddNewPlace
             key = "addPlace"
-            onPlaceSubmit={this.handlePlaceSubmit}
           />
-
-        } 
-
+        }  
       </div>
-
     )
   }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    zip: state.zip,
+    city: state.city
+    
+  };
+}
+
+export default connect(mapStateToProps)(App);
